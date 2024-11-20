e5091f5419a33c75367652e9626a924df2da3928
riccardo <riccardo@tostapane-opensource.homenet.telecomitalia.it>
2024-11-20 14:52:22 +0100

    first commmit and idea

---
+++ PotatoTester.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+detect
+
+select_expected_output()
+{
+    for arg in "$@"; do
+
+}
+
+
+#ls user_scripts
+#if diff "expected_outputs/test" "expected_outputs/test2" > /dev/null; then
+#    echo "\033[1;4;32mVALID!\033[0m"
+#fi
